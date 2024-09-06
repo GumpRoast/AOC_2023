3e1371f75c5478243ade96d6534218be8355b77c
GumpRoast <33532206+GumpRoast@users.noreply.github.com>
2024-09-05 20:45:35 -0400

    change

---
+++ 1-1.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+path = str(Path(__file__).parent)
+file_path = path + '/1_ex.txt'
+
+with open(file_path, 'r') as f:
+    for line in f:
+        for item in line:
+            if item.isnumeric():
+                store = item
+            print(store)
+
+            
