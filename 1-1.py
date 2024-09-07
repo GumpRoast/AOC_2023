77828d61ed48b8b34528f9af2536fb04bcd4ba16
GumpRoast <33532206+GumpRoast@users.noreply.github.com>
2024-09-07 00:20:06 +0000

    modified:   1-1.py

--- 1-1.py
+++ 1-1.py
@@ -1,14 +1,31 @@
 from pathlib import Path
 
 path = str(Path(__file__).parent)
-file_path = path + '/1_ex.txt'
+file_path = path + '/1_input.txt'
+
+nums = []
 
 with open(file_path, 'r') as f:
     for line in f:
+
+        line_r = reversed(line)
+
         for item in line:
             if item.isnumeric():
                 store = item
                 break
+        #print(store)
         
-        print(store)
+        for item2 in line_r:
+            if item2.isnumeric():
+                store2 = item2
+                break
+        #print(store2)
 
+        nums.append(store + store2)
+total = 0
+for num in nums:
+    total += int(num)
+
+print(total)
+  
